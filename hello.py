a566d0358dbfbd9217441f26e6988578324c7ae3
Julio Estefanes <jsgr1992@gmail.com>
2021-03-17 20:26:21 -0600

    primer commit y subir a master

---
+++ hello.py
@@ -0,0 +1,25 @@
+msg="Julio"
+print(msg)
+
+msg="new message"
+print(msg)
+
+# myName = input()
+# print("The length on your name is:")
+# print(len(myName))
+
+print(str(343434))
+print(int(1.99))
+float() #decimal
+
+#boleean
+spam = True
+print(spam)
+
+num1 = 5
+num2 = 10
+
+#comparison operators
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
