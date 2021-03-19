e9f08025a744edf3b78793069451d3e48f1a4a1a
Julio Estefanes <jsgr1992@gmail.com>
2021-03-18 19:52:08 -0600

    if-examples

--- hello.py
+++ hello.py
@@ -1,25 +1,26 @@
-msg="Julio"
-print(msg)
+# msg="Julio"
+# print(msg)
 
-msg="new message"
-print(msg)
+# msg="new message"
+# print(msg)
 
 # myName = input()
 # print("The length on your name is:")
 # print(len(myName))
 
-print(str(343434))
-print(int(1.99))
+# print(str(343434))
+# print(int(1.99))
 float() #decimal
 
 #boleean
-spam = True
-print(spam)
+# spam = True
+# print(spam)
 
-num1 = 5
-num2 = 10
+# num1 = 5
+# num2 = 10
 
 #comparison operators
-print(num1 == num2)
-print(num1 != num2)
-print(num1 >= num2)
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 >= num2) 
+
---
+++ if-else.py
@@ -0,0 +1,18 @@
+## if statements
+# value = 5
+# if value > 1:
+#     print('Mayor que 1')
+#     value = 10
+
+# print('fuera del if')    
+# print(value)
+
+
+condition = False
+
+if condition == True:
+    print("The condition")
+    print("was True")
+else:
+    print("The condition")
+    print("was False")
